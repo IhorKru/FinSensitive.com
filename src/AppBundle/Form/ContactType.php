67cf051449decfe09a50934313fd01833fdff18d
ihorkruchynenko <ihorkruchynenko@10.0.0.7>
2016-07-03 11:36:28 +0200

    Great commit

---
+++ src/AppBundle/Form/ContactType.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ContactType
+ *
+ * @author ihorkruchynenko
+ */
+class ContactType {
+    //put your code here
+}
